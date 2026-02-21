1d931cd9191372d28e35d1cfade04462da0fd452
Sreedev <sreedevnp5@gmail.com>
2026-02-21 11:06:19 +0530

    Removed email credentials from settings

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -132,8 +132,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'showgunlab@gmail.com'
-EMAIL_HOST_PASSWORD = 'eaqp yzxk mioe sbey'
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
 
 
 # Default primary key field type
